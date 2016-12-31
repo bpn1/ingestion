03426dfd08bfac53601ecb246889f2914488b9e9
Lando-L <lando.loeper@gmx.net>
2016-12-31 03:32:01 +0100

    DataLakeImport copied Import from other Repo and created abstract DataLakeImport class

---
+++ DataLakeImport/src/main/scala/DataLakeImportWikidata.scala
@@ -0,0 +1,42 @@
+import scala.collection.mutable
+
+object DataLakeImportWikidata extends DataLakeImport(
+	"DataLakeImportWikidata_v1.0",
+	List("wikidata_20161117"),
+	"wikidumps",
+	"wikidata"
+){
+	case class WikiDataEntity(
+		var id: String = "",
+		var entitytype: Option[String] = None,
+		var instancetype: Option[String] = None,
+		var wikiname: Option[String] = None,
+		var description: Option[String] = None,
+		var label: Option[String] = None,
+		var aliases: List[String] = List[String](),
+		var data: Map[String, List[String]] = Map[String, List[String]]()
+	)
+
+	override type T = WikiDataEntity
+
+	def translateToSubject(entity: WikiDataEntity, version: Version) : Subject = {
+		val subject = Subject()
+		val sm = new SubjectManager(subject, version)
+
+		if(entity.label.isDefined)
+			sm.setName(entity.label.get)
+		if(entity.aliases.nonEmpty)
+			sm.addAliases(entity.aliases)
+		if(entity.instancetype.isDefined)
+			sm.setCategory(entity.instancetype.get)
+
+		val metadata = mutable.Map[String, List[String]](("wikidata_id", List(entity.id)))
+		if(entity.wikiname.isDefined)
+			metadata += "wikipedia_name" -> List(entity.wikiname.get)
+		if(entity.data.nonEmpty)
+			metadata ++= entity.data
+
+		sm.addProperties(metadata.toMap)
+		subject
+	}
+}
