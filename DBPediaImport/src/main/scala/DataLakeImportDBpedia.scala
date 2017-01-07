3dfceb0f3a97a2d98380d1ffa1631bf47bc066ea
Lando-L <lando.loeper@gmx.net>
2017-01-07 18:09:11 +0100

    Issue 3#: Added DataLakeImport file

---
+++ DBPediaImport/src/main/scala/DataLakeImportDBpedia.scala
@@ -0,0 +1,30 @@
+import DataLake.{DataLakeImport, Subject, SubjectManager, Version}
+import scala.collection.mutable
+
+object DataLakeImportDBpedia extends DataLakeImport[DBPediaEntity](
+	"DataLakeImportDBpedia_v1.0",
+	List("dbpedia"),
+	"wikidumps",
+	"dbpedia"
+){
+	override def translateToSubject(entity: DBPediaEntity, version: Version): Subject = {
+		val subject = Subject()
+		val sm = new SubjectManager(subject, version)
+
+		if(entity.label.isDefined)
+			sm.setName(entity.label.get.head)
+
+		val metadata = mutable.Map[String, List[String]]("wikipageId" -> List(entity.wikipageId), "dbpediaName" -> List(entity.dbPediaName))
+		if(entity.description.isDefined)
+			metadata += "description" -> List(entity.description.get.head)
+		if(entity.data.nonEmpty)
+			metadata ++= entity.data
+
+		sm.addProperties(metadata.toMap)
+		subject
+	}
+
+	def main(args: Array[String]): Unit = {
+		importToCassandra()
+	}
+}
