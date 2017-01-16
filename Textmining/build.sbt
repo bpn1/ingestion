e97ce8e208a03e96c66f115ad01b4422943b35ca
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-01-16 16:44:46 +0100

    uses java wikipedia api instead of sweble

--- Textmining/build.sbt
+++ Textmining/build.sbt
@@ -1,27 +1,28 @@
 lazy val root = (project in file(".")).
-  settings(
-    name := "Textmining",
-    version := "1.0",
-    scalaVersion := "2.10.5",
-    mainClass in Compile := Some("Textmining")
-  )
+	settings(
+		name := "Textmining",
+		version := "1.0",
+		scalaVersion := "2.10.5",
+		mainClass in Compile := Some("Textmining")
+	)
 
 resolvers ++= Seq(
-  "Spark Packages Repo" at "https://dl.bintray.com/spark-packages/maven",
-  "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+	"Spark Packages Repo" at "https://dl.bintray.com/spark-packages/maven",
+	"Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
 )
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.6.2",
-  "com.datastax.spark" %% "spark-cassandra-connector" % "1.6.1",
-  "org.apache.spark" % "spark-sql_2.10" % "1.6.2",
-  "com.datastax.cassandra" % "cassandra-driver-core" % "3.1.2",
-  "com.typesafe.play" %% "play-json" % "2.3.0",
-  "org.scalactic" %% "scalactic" % "3.0.1",
-  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
-  "com.holdenkarau" %% "spark-testing-base" % "1.6.1_0.3.3",
-  "com.databricks" % "spark-xml_2.10" % "0.4.1",
-  "org.sweble.wikitext" % "swc-engine" % "2.2.0"
+	"org.apache.spark" %% "spark-core" % "1.6.2",
+	"com.datastax.spark" %% "spark-cassandra-connector" % "1.6.1",
+	"org.apache.spark" % "spark-sql_2.10" % "1.6.2",
+	"com.datastax.cassandra" % "cassandra-driver-core" % "3.1.2",
+	"com.typesafe.play" %% "play-json" % "2.3.0",
+	"org.scalactic" %% "scalactic" % "3.0.1",
+	"org.scalatest" %% "scalatest" % "3.0.1" % "test",
+	"com.holdenkarau" %% "spark-testing-base" % "1.6.1_0.3.3",
+	"com.databricks" % "spark-xml_2.10" % "0.4.1",
+	"org.sweble.wikitext" % "swc-engine" % "2.2.0",
+	"info.bliki.wiki" % "bliki-core" % "3.1.0"
 )
 
 // testing settings
@@ -33,12 +34,12 @@ javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:MaxPermSize=2048M", "-XX:+CMSC
 
 // fat jar assembly settings
 mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) => {
-    case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-    case PathList(ps @ _*) if ps.last endsWith "pom.properties" => MergeStrategy.discard
-    case x => MergeStrategy.first
-  }
+		case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+		case PathList(ps @ _*) if ps.last endsWith "pom.properties" => MergeStrategy.discard
+		case x => MergeStrategy.first
+	}
 }
 
 assemblyShadeRules in assembly := Seq(
-  ShadeRule.rename("com.google.**" -> "shadeio.@1").inAll
+	ShadeRule.rename("com.google.**" -> "shadeio.@1").inAll
 )
--- Textmining/src/main/scala/WikipediaTextparser.scala
+++ Textmining/src/main/scala/WikipediaTextparser.scala
@@ -8,6 +8,7 @@ import scala.collection.JavaConversions._
 import scala.collection.mutable
 import org.sweble.wikitext.parser.nodes._
 import scala.util.matching.Regex
+import info.bliki.wiki.model.WikiModel;
 
 object WikipediaTextparser {
     val keyspace = "wikidumps"
@@ -18,114 +19,15 @@ object WikipediaTextparser {
         var text: String,
         var links: List[Map[String, String]])
 
-    def parseWikipediaPage(entry: WikipediaEntry): EngPage = {
-        val config = DefaultConfigEnWp.generate()
-        val compiler = new WtEngineImpl(config)
-        val pageTitle = PageTitle.make(config, entry.title)
-        val pageId = new PageId(pageTitle, 0)
-        val page = compiler.postprocess(pageId, entry.text, null).getPage
-        page
-    }
+	def wikipediaToHtml(entry: WikipediaEntry): WikipediaEntry = {
+		val cleanText = entry.text.replaceAll("\\\\n", "\n")
+		entry.text = WikiModel.toHtml(cleanText)
+		entry
+	}
+	def cleanTables(entry: WikipediaEntry): WikipediaEntry = {
+		val tableRegex = new Regex("<table(.*\n)*?.*?</table>")
+	}
 
-    def parseTree(page: EngPage): (String, List[Map[String, String]]) = {
-        val result = traversePageTree(page, 0)
-        println(s"\npage:")
-        println(result._1)
-        println(result._2.mkString("\n"))
-        result
-    }
-
-    // traverse all children
-    def traverseChildren(node: WtNode, offset: Int): (String, List[Map[String, String]]) = {
-        var text = ""
-        val linkList = mutable.ListBuffer[Map[String, String]]()
-        if (node.iterator == null)
-            return (text, linkList.toList)
-        for (element <- node.iterator.toList) {
-            val (resultText, resultList) = traversePageTree(element, offset + text.length)
-            text += resultText
-            linkList ++= resultList
-        }
-        (text, linkList.toList)
-    }
-
-    def traversePageTree(node: WtNode, offset: Int): (String, List[Map[String, String]]) = {
-        val fileRegex = new Regex("^Datei:")
-        val wikimarkupRegex = new Regex("(^\\[\\[.*\\]\\]$|(\\[\\[|\\]\\]))")
-        var text = ""
-        val linkList = mutable.ListBuffer[Map[String, String]]()
-
-        node match {
-            // try case t: EngPage | t: WtParagraph
-            case t: EngPage => traverseChildren(t, offset)
-
-            case t: WtParagraph => traverseChildren(t, offset)
-
-            case t: WtBold => traverseChildren(t, offset)
-
-            case t: WtItalics => traverseChildren(t, offset)
-
-            case t: WtExternalLink => (text, linkList.toList)
-
-            case t: WtXmlElement => (text, linkList.toList)
-
-            case t: WtTagExtension => (text, linkList.toList)
-
-            case t: WtTemplate =>
-                // check [0], if Infobox discard
-                // else ?
-                (text, linkList.toList)
-
-            case t: WtText =>
-                // extract text
-                //val tmpText = t.getContent.replaceAll("\\\\n", "\n")
-                val tmpText = t.getContent
-                    .replaceAll("\\.\\\\n", ". ")
-                    .replaceAll("\\\\n", "")
-                if (wikimarkupRegex.findFirstIn(tmpText) == None && fileRegex.findFirstIn(tmpText) == None)
-                    text += tmpText
-                text
-                (text, linkList.toList)
-
-            case t: WtInternalLink =>
-                // [0] = target (WtPagename)
-                // [1] = source text (WtLinkTitle or WtNoLinkTitle)
-                val link = mutable.Map[String, String]()
-                var source = ""
-                var target = ""
-                if (t.iterator != null) {
-                    for (prop <- t.iterator.toList) {
-                        prop match {
-                            case p: WtPageName =>
-                                target = p(0).asInstanceOf[WtText].getContent
-                                if (fileRegex.findFirstIn(target) != None)
-                                    return (text, linkList.toList)
-                            case p: WtLinkTitle =>
-                                val contentList = p.iterator.toList
-                                for (textElement <- contentList) {
-                                    val tmpText = textElement.asInstanceOf[WtText].getContent.replaceAll("\\\\n", "\n")
-                                    source += tmpText
-                                }
-                            case _ =>
-                        }
-                    }
-                }
-
-                if (source == "")
-                    source = target
-                link("source") = source
-                link("target") = target
-                link("char_offset_begin") = offset.toString
-                link("char_offset_end") = (offset + source.length).toString
-                linkList += link.toMap
-                text += source
-                (text, linkList.toList)
-
-            case _ =>
-                println(s"\n\nfail: $node.iterator.toList \n\n")
-                (text, linkList.toList)
-        }
-    }
 
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf()
@@ -134,14 +36,11 @@ object WikipediaTextparser {
         val sc = new SparkContext(conf)
 
         val wikipedia = sc.cassandraTable[WikipediaEntry](keyspace, tablename)
-        wikipedia
-            .map { case wikipediaEntry =>
-                val page = parseWikipediaPage(wikipediaEntry)
-                val parsedData = parseTree(page)
-                wikipediaEntry.text = parsedData._1
-                wikipediaEntry.links = parsedData._2
-                wikipediaEntry
-            }.saveToCassandra(keyspace, tablename)
-        sc.stop
+		/*
+		wikipedia
+            .map(wikipediaToHtml)
+			.saveToCassandra(keyspace, tablename)
+		*/
+		sc.stop
     }
 }
