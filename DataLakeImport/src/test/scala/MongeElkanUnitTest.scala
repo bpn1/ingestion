330240b4e70ff887d98d516d537f2b53ccaa3880
Nils Strelow <nils.strelow@student.hpi.de>
2017-02-06 15:16:09 +0100

    score was renamed to compare

--- DataLakeImport/src/test/scala/MongeElkanUnitTest.scala
+++ DataLakeImport/src/test/scala/MongeElkanUnitTest.scala
@@ -5,22 +5,22 @@ import org.scalatest.FlatSpec
 
 class MongeElkanUnitTest extends FlatSpec {
 
-	"max" should "return 0.0 if given list is empty" in {
-			val maximum = MongeElkan.max(List())
-			assert(maximum == 0.0)
-	}
-	it should "return the maximum of a given list" in {
-		val maximum = MongeElkan.max(List(12.2, 2.5, 34.8, 11.0, 0.5))
-		assert(maximum == 34.8)
-	}
+  "max" should "return 0.0 if given list is empty" in {
+    val maximum = MongeElkan.max(List())
+    assert(maximum == 0.0)
+  }
+  it should "return the maximum of a given list" in {
+    val maximum = MongeElkan.max(List(12.2, 2.5, 34.8, 11.0, 0.5))
+    assert(maximum == 34.8)
+  }
 
-	"maxSim" should "return the score with the highest similarity" in {
-		val maximum = MongeElkan.maxSim("henka", List("henkan", "123", "xyz"))
-		assert(maximum == JaroWinklerMetric.compare("henka", "henkan").get)
-	}
+  "maxSim" should "return the score with the highest similarity" in {
+    val maximum = MongeElkan.maxSim("henka", List("henkan", "123", "xyz"))
+    assert(maximum == JaroWinklerMetric.compare("henka", "henkan").get)
+  }
 
-	"score" should "return the MongeElkan score for given strings" in {
-		val score = MongeElkan.score("henka", "henkan")
-		assert(score == JaroWinklerMetric.compare("henka", "henkan").get)
-	}
+  "score" should "return the MongeElkan score for given strings" in {
+    val score = MongeElkan.compare("henka", "henkan")
+    assert(score == JaroWinklerMetric.compare("henka", "henkan").get)
+  }
 }
