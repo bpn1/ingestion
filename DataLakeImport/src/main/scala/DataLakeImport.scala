ce91aefa56cdbfca0ca11600c05e6464b48a138b
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-02-11 03:21:44 +0100

    Refs #30: updates to scala 2.11 and new cluster

--- DataLakeImport/src/main/scala/DataLakeImport.scala
+++ DataLakeImport/src/main/scala/DataLakeImport.scala
@@ -31,15 +31,15 @@ abstract class DataLakeImport[T <: Serializable : ClassTag : RowReaderFactory](
 	protected def importToCassandra(): Unit = {
 		val conf = new SparkConf()
 			.setAppName(appName)
-			.set("spark.cassandra.connection.host", "172.20.21.11")
+			.set("spark.cassandra.connection.host", "odin01")
 		val sc = new SparkContext(conf)
 
 		val data = sc.cassandraTable[T](inputKeyspace, inputTable)
 		val version = makeTemplateVersion()
 
 		data
-		  .map(translateToSubject(_, version))
-		  .saveToCassandra(outputKeyspace, outputTable)
+			.map(translateToSubject(_, version))
+			.saveToCassandra(outputKeyspace, outputTable)
 
 		sc
 			.parallelize(List((version.version, version.timestamp, version.datasources, version.program)))
