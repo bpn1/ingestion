dd366086e74656f074869f7a118bf07eaf4f0d8d
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-02-19 23:19:42 +0100

    removes unnecessary file and dead code

--- DBPediaImport/src/test/scala/DBpediaUnitTest.scala
+++ DBPediaImport/src/test/scala/DBpediaUnitTest.scala
@@ -39,56 +39,6 @@ class DBpediaUnitTest extends FlatSpec with SharedSparkContext {
 
 	// TODO test extractProperties()
 
-//	it should "contain the subject as property" in {
-//		val properties = DBPediaImport.extractProperties(group)
-//		assert(properties.exists(tuple => tuple._1 == "dbpedia-entity" && tuple._2 == group._1))
-//	}
-//
-//	"createMap" should "return a not empty Map" in {
-//		val map = DBPediaImport.createMap(properties)
-//		assert(map.isInstanceOf[Map[String, List[String]]])
-//		assert(map.nonEmpty)
-//	}
-//
-//	it should "contain a key->value pair for each unique predicate" in {
-//		val map = DBPediaImport.createMap(properties)
-//		assert(map.size == 3)
-//	}
-//
-//	"translateToDBpediaEntity" should "return a DBpediaEntity" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		assert(entity.isInstanceOf[DBPediaEntity])
-//	}
-//
-//	it should "have a wikipageId" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		assert(entity.wikipageid.nonEmpty)
-//	}
-//
-//	it should "have a dbPediaName" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		assert(entity.dbpedianame.nonEmpty)
-//	}
-//
-//	it should "have a label optionally" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		val entityWithoutLabel = DBPediaImport.translateToDBPediaEntry(mapWithoutLabelAndDescription)
-//		assert(entity.label.isDefined)
-//		assert(entityWithoutLabel.label.isEmpty)
-//	}
-//
-//	it should "have a description optionally" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		val entityWithoutDescription = DBPediaImport.translateToDBPediaEntry(mapWithoutLabelAndDescription)
-//		assert(entity.description.isDefined)
-//		assert(entityWithoutDescription.description.isEmpty)
-//	}
-//
-//	it should "have a data Map for other properties" in {
-//		val entity = DBPediaImport.translateToDBPediaEntry(map)
-//		assert(entity.data.size == 3)
-//	}
-
 	def prefixesList(): List[(String,String)] = {
 		val prefixFile = Source.fromURL(getClass.getResource("/prefixes.txt"))
 		val prefixes = prefixFile.getLines.toList
