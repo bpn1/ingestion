47235826e6e246e8a5de2d0c3a7ec4d5da610c9f
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-02-19 16:14:43 +0100

    Refs #31: fixes serialization issue

--- DataLakeImport/src/main/scala/DataLakeImport.scala
+++ DataLakeImport/src/main/scala/DataLakeImport.scala
@@ -3,7 +3,7 @@ package DataLake
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 import java.util.Date
-
+import org.apache.spark.rdd.RDD
 import com.datastax.driver.core.utils.UUIDs
 import com.datastax.spark.connector.rdd.reader.RowReaderFactory
 import scala.reflect.ClassTag
@@ -12,6 +12,7 @@ trait DLImport[T] extends Serializable {
 	val outputKeyspace = "datalake"
 	val outputTable = "subject_temp"
 	val versionTable = "version"
+	protected def readInput(sc: SparkContext, version: Version): RDD[Subject]
 	protected def translateToSubject(entity: T, version: Version): Subject
 	protected def makeTemplateVersion(): Version
 	protected def importToCassandra(): Unit
@@ -37,13 +38,11 @@ abstract case class DataLakeImport[T <: Serializable : ClassTag : RowReaderFacto
 			.set("spark.cassandra.connection.host", "odin01")
 		val sc = new SparkContext(conf)
 
-		val data = sc.cassandraTable[T](inputKeyspace, inputTable)
+		//val data = sc.cassandraTable[T](inputKeyspace, inputTable)
 		val version = makeTemplateVersion()
-
-		data
-		  .map(translateToSubject(_, version))
-		  .saveToCassandra(outputKeyspace, outputTable)
-
+		val data = readInput(sc, version)
+		data.saveToCassandra(outputKeyspace, outputTable)
+		//   .map(translateToSubject(_, version))
 		sc
 			.parallelize(List((version.version, version.timestamp, version.datasources, version.program)))
 			.saveToCassandra(outputKeyspace, versionTable, SomeColumns("version", "timestamp", "datasources", "program"))
