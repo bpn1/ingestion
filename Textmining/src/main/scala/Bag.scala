4aa7539b2e143bb4bd56feac2112f54a9885e8a4
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-03-10 00:52:12 +0100

    Closes #74: Feature/textmining/74 (#100)
    * implemented continous counts for Bag, #74
    
    * Refs #74: implements second type parameter for bag
    
    * Refs #74, 63: updates scalastyle config
    
    * Refs #74: fixes Bag and implements proper ++/-- methods
    
    * Refs #74: adds normalise method
    
    * Refs #74: removes accidentally added file
    
    * Resfs #74: removes println

--- Textmining/src/main/scala/Bag.scala
+++ Textmining/src/main/scala/Bag.scala
@@ -1,33 +1,165 @@
 import scala.collection.immutable.Set
 import scala.collection.SetLike
 import scala.collection.mutable.Builder
+import scala.language.implicitConversions
+import scala.reflect.{ClassTag, classTag}
+import scala.math.BigDecimal
 
-class Bag[A](
-	private val counts: Map[A, Int] = Map.empty[A, Int]) extends SetLike[A, Bag[A]] with Set[A]
+trait BagCounter[T] extends Ordered[T]{
+	def +(that: T): T
+	def -(that: T): T
+	def *(that: T): T
+	def /(that: T): T
+	def equalsZero(): Boolean
+	def lessThanZero(): Boolean
+	def invert(): T
+	def round(): T
+	def compare(that: T): Int
+}
+
+object BagCounter {
+	implicit def intToBagCounter(x : Int): BagCounter[Int] = new BagCounter[Int] {
+		def +(y : Int): Int = x + y
+		def -(y : Int): Int = x - y
+		def *(y : Int): Int = x * y
+		def /(y : Int): Int = x / y
+		def equalsZero(): Boolean = x == 0
+		def lessThanZero(): Boolean = x < 0
+		def invert(): Int = x * -1
+		def round(): Int = x
+		def compare(y: Int): Int = x - y
+	}
+
+	implicit def doubleToBagCounter(x : Double): BagCounter[Double] = new BagCounter[Double] {
+		def +(y : Double): Double = x + y
+		def -(y : Double): Double = x - y
+		def *(y : Double): Double = x * y
+		def /(y : Double): Double = x / y
+		def equalsZero(): Boolean = x == 0.0
+		def lessThanZero(): Boolean = x < 0.0
+		def invert(): Double = x * -1.0
+		// TODO set precision
+		def round(): Double = BigDecimal(x).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+		def compare(y: Double): Int = (x - y).signum
+	}
+}
+
+class Bag[A, B <% BagCounter[B] : ClassTag](
+	private val counts: Map[A, B] = Map.empty[A, B]) extends SetLike[A, Bag[A, B]] with Set[A]
 {
-
 	// http://stackoverflow.com/questions/15065070/implement-a-multiset-bag-as-scala-collection
 
-	override def +(elem: A): Bag[A] = {
-		val count = this.counts.getOrElse(elem, 0) + 1
+	def getCounts(): Map[A, B] = counts
+
+	override def +(elem: A): Bag[A, B] = {
+		val count = this.counts.get(elem) match {
+			case Some(x) => {
+				x match {
+					case t: Int => (t + 1).asInstanceOf[B]
+					case t: Double => (t + 1.0).asInstanceOf[B]
+				}
+			}
+			case None => {
+				if(classTag[B] == classTag[Int] || classTag[B] == classTag[Integer]) {
+					1.asInstanceOf[B]
+				} else {
+					1.0.asInstanceOf[B]
+				}
+			}
+		}
 		new Bag(this.counts + (elem -> count))
 	}
 
-	override def -(elem: A): Bag[A] = this.counts.get(elem) match {
+	def +(elem: A, times: B): Bag[A, B] = {
+		if(times.lessThanZero()) {
+			return this - (elem, times.invert())
+		}
+		if(times.equalsZero()) {
+			return this
+		}
+		val count = this.counts.get(elem) match {
+			case Some(x) => (x + times).round
+			case None => times
+		}
+		new Bag(this.counts + (elem -> count))
+	}
+
+	def ++(that: Bag[A, B]): Bag[A, B] = {
+		var result = this
+		for((elem: A @unchecked, count: B) <- that.getCounts()) {
+			result = result + (elem, count)
+		}
+		result
+	}
+
+	override def -(elem: A): Bag[A, B] = this.counts.get(elem) match {
 		case None => this
-		case Some(1) => new Bag(this.counts - elem)
-		case Some(n) => new Bag(this.counts + (elem -> (n - 1)))
+		case Some(x) => {
+			x match {
+				case t: Int => {
+					if(t == 1) {
+						new Bag(this.counts - elem)
+					} else {
+						new Bag(this.counts + (elem -> (t - 1).asInstanceOf[B]))
+					}
+				}
+				case t: Double => {
+					val newCount = t - 1.0
+					if(newCount <= 0.0) {
+						new Bag(this.counts - elem)
+					} else {
+						new Bag(this.counts + (elem -> (t - 1.0).asInstanceOf[B]))
+					}
+				}
+			}
+		}
+	}
+
+	def -(elem: A, times: B): Bag[A, B] = {
+		if(times.lessThanZero) {
+			return this + (elem, times.invert())
+		}
+		if(times.equalsZero) {
+			return this
+		}
+		this.counts.get(elem) match {
+			case None => this
+			case Some(x) => {
+				val newCount = (x - times).round
+				if(newCount.lessThanZero()) {
+					new Bag(this.counts - elem)
+				} else {
+					new Bag(this.counts + (elem -> newCount))
+				}
+			}
+		}
+	}
+
+	def --(that: Bag[A, B]): Bag[A, B] = {
+		var result = this
+		for((elem: A @unchecked, count: B) <- that.getCounts()) {
+			result = result - (elem, count)
+		}
+		result
 	}
 
 	override def contains(elem: A): Boolean = this.counts.contains(elem)
 
-	override def empty: Bag[A] = new Bag[A]
+	override def empty: Bag[A, B] = new Bag[A, B]
 
-	override def iterator: Iterator[A] = {
-		for ((elem, count) <- this.counts.iterator; _ <- 1 to count) yield elem
+	def elementSize(count: B): Int = {
+		if(classTag[B] == classTag[Double]) {
+			count.asInstanceOf[Double].intValue()
+		} else {
+			count.asInstanceOf[Int]
+		}
 	}
 
-	override def newBuilder: Builder[A, Bag[A]] = new Builder[A, Bag[A]] {
+	override def iterator: Iterator[A] = {
+		for((elem, count) <- this.counts.iterator; _ <- 1 to elementSize(count)) yield elem
+	}
+
+	override def newBuilder: Builder[A, Bag[A, B]] = new Builder[A, Bag[A, B]] {
 		var Bag = empty
 
 		def +=(elem: A): this.type = {
@@ -37,20 +169,74 @@ class Bag[A](
 
 		def clear(): Unit = this.Bag = empty
 
-		def result(): Bag[A] = this.Bag
+		def result(): Bag[A, B] = this.Bag
 	}
 
-	override def seq: Bag[A] = this
+	override def seq: Bag[A, B] = this
+
+	override def equals(that: Any): Boolean = {
+		this.counts == that.asInstanceOf[Bag[A, B]].getCounts()
+	}
+
+	override def hashCode(): Int = this.counts.hashCode
+
+	override def toString(): String = this.counts.toString.replaceAll("Map", "Bag")
+
+	def normalise(): Bag[A, Double] = {
+		var result = new Bag[A, Double]
+		val maxCount = this.counts.maxBy(_._2)._2 match {
+			case x: Int => x.doubleValue()
+			case x: Double => x
+		}
+		for((elem, count) <- this.counts) {
+			val newValue = (count match {
+				case x: Int => x.doubleValue()
+				case x: Double => x
+			}) / maxCount
+			result = result + (elem, newValue)
+		}
+		result
+	}
+
 }
 
 object Bag {
-	def empty[A]: Bag[A] = new Bag[A]
+	def empty[A, B : ClassTag]
+		(implicit bToBagCounter: B => BagCounter[B])
+	: Bag[A, B] =
+	{
+		new Bag[A, B]
+	}
 
-	def apply[A](elem: A, elems: A*): Bag[A] = Bag.empty + elem ++ elems
+	def apply[A, B : ClassTag]
+		(elem: A, elems: A*)
+		(implicit bToBagCounter: B => BagCounter[B])
+	: Bag[A, B] =
+	{
+		new Bag[A, B] + elem ++ elems
+	}
 
-	def apply[A](elems: Seq[A]): Bag[A] = Bag.empty ++ elems
+	def apply[A, B : ClassTag]
+		(elems: Seq[A])
+			(implicit bToBagCounter: B => BagCounter[B])
+	: Bag[A, B] =
+	{
+		new Bag[A, B] ++ elems
+	}
 
-	def apply[A](elem: (A, Int), elems: (A, Int)*) = new Bag((elem +: elems).toMap)
+	def apply[A, B : ClassTag]
+		(elem: (A, B), elems: (A, B)*)
+		(implicit bToBagCounter: B => BagCounter[B])
+	: Bag[A, B] =
+	{
+		new Bag((elem +: elems).toMap)
+	}
 
-	def apply[A](elems: Map[A, Int]): Bag[A] = new Bag(elems)
+	def apply[A, B : ClassTag]
+		(elems: Map[A, B])
+		(implicit bToBagCounter: B => BagCounter[B])
+	: Bag[A, B] =
+	{
+		new Bag(elems)
+	}
 }
--- Textmining/src/test/scala/BagTest.scala
+++ Textmining/src/test/scala/BagTest.scala
@@ -1,28 +1,72 @@
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
 
-class BagTest extends FlatSpec {
+class BagTest extends FlatSpec with Matchers {
 	// http://stackoverflow.com/questions/15065070/implement-a-Bag-bag-as-scala-collection
+
 	"Bag" should "add elements properly" in {
-		assert(Bag("X" -> 3, "Y" -> 1) + "X" ===
-			Bag("X" -> 4, "Y" -> 1))
-		assert(Bag("X" -> 3, "Y" -> 1) + "Z" ===
-			Bag("X" -> 3, "Y" -> 1, "Z" -> 1))
+		Bag("X" -> 3, "Y" -> 1) + "X" shouldEqual Bag("X" -> 4, "Y" -> 1)
+		Bag("X" -> 3, "Y" -> 1) + "Z" shouldEqual Bag("X" -> 3, "Y" -> 1, "Z" -> 1)
+
+		Bag("X" -> 3.5, "Y" -> 1.3) + "X" shouldEqual Bag("X" -> 4.5, "Y" -> 1.3)
+		Bag("X" -> 3.4, "Y" -> 1.3) + "Z" shouldEqual Bag("X" -> 3.4, "Y" -> 1.3, "Z" -> 1.0)
+
+	}
+
+	it should "add elements with number of occurrences properly" in {
+		Bag("X" -> 3, "Y" -> 1) + ("X", 3) shouldEqual Bag("X" -> 6, "Y" -> 1)
+		Bag("X" -> 3, "Y" -> 1) + ("Z", 2) shouldEqual Bag("X" -> 3, "Y" -> 1, "Z" -> 2)
+		Bag("X" -> 3, "Y" -> 1) + ("Z", 0) shouldEqual Bag("X" -> 3, "Y" -> 1)
+
+		Bag("X" -> 3.5, "Y" -> 1.3) + ("X", 1.2) shouldEqual Bag("X" -> 4.7, "Y" -> 1.3)
+		Bag("X" -> 3.4, "Y" -> 1.3) + ("Z", 2.2) shouldEqual Bag("X" -> 3.4, "Y" -> 1.3, "Z" -> 2.2)
 	}
 
 	it should "remove elements properly" in {
-		assert(Bag("a" -> 2, "b" -> 5) - "b" ===
-			Bag("a" -> 2, "b" -> 4))
-		assert(Bag("a" -> 2, "b" -> 5) - "c" ===
-			Bag("a" -> 2, "b" -> 5))
+		Bag("a" -> 2, "b" -> 5) - "b" shouldEqual Bag("a" -> 2, "b" -> 4)
+		Bag("a" -> 2, "b" -> 5) - "c" shouldEqual Bag("a" -> 2, "b" -> 5)
+
+		Bag("X" -> 3.5, "Y" -> 1.3) - "X" shouldEqual Bag("X" -> 2.5, "Y" -> 1.3)
+		Bag("X" -> 3.4, "Y" -> 1.3) - "Z" shouldEqual Bag("X" -> 3.4, "Y" -> 1.3)
+	}
+
+	it should "remove elements with number of occurrences properly" in {
+		Bag("X" -> 3, "Y" -> 1) - ("X", 2) shouldEqual Bag("X" -> 1, "Y" -> 1)
+		Bag("X" -> 3, "Y" -> 1) - ("Y", 2) shouldEqual Bag("X" -> 3)
+		Bag("X" -> 3, "Y" -> 1) - ("Z", 0) shouldEqual Bag("X" -> 3, "Y" -> 1)
+
+		Bag("X" -> 3.5, "Y" -> 1.3) - ("X", 2.6) shouldEqual Bag("X" -> 0.9, "Y" -> 1.3)
+		Bag("X" -> 3.5, "Y" -> 1.3) - ("Y", 1.4) shouldEqual Bag("X" -> 3.5)
+		Bag("X" -> 3.4, "Y" -> 1.3) - ("Z", 2.2) shouldEqual Bag("X" -> 3.4, "Y" -> 1.3)
 	}
 
 	"Bag sum" should "be correct" in {
-		assert(Bag(10 -> 1, 100 -> 3) ++ Bag(10 -> 1, 1 -> 7) ===
-			Bag(100 -> 3, 10 -> 2, 1 -> 7))
+		val res1 = Bag(100 -> 3, 10 -> 2, 1 -> 7)
+		val res2 = Bag("X" -> 5, "Y" -> 4, "Z" -> 1)
+		val res3 = Bag("X" -> 5.7, "Y" -> 4.3, "Z" -> 1.3)
+		Bag(10 -> 1, 100 -> 3) ++ Bag(10 -> 1, 1 -> 7) shouldEqual res1
+		Bag("X" -> 3, "Y" -> 1) ++ Bag("X" -> 2, "Y" -> 3, "Z" -> 1) shouldEqual res2
+		Bag("X" -> 3.4, "Y" -> 1.3) ++ Bag("X" -> 2.3, "Y" -> 3.0, "Z" -> 1.3) shouldEqual res3
 	}
 
 	"Bag difference" should "be correct" in {
-		assert(Bag("a" -> 2, "b" -> 5) -- Bag("a" -> 3) ===
-			Bag("b" -> 5))
+		val res1 = Bag("b" -> 5)
+		val res2 = Bag("X" -> 1, "Y" -> 1)
+		val res3 = Bag("X" -> 0.6, "Y" -> 1.2)
+		val res4 = Bag("X" -> 1.6)
+		Bag("a" -> 2, "b" -> 5) -- Bag("a" -> 3) shouldEqual res1
+		Bag("X" -> 3, "Y" -> 1) -- Bag("X" -> 2, "Z" -> 1) shouldEqual res2
+		Bag("X" -> 3.4, "Y" -> 1.3) -- Bag("X" -> 2.8, "Y" -> 0.1, "Z" -> 0.5) shouldEqual res3
+		Bag("X" -> 3.4, "Y" -> 1.3) -- Bag("X" -> 1.8, "Y" -> 1.4, "Z" -> 1.3) shouldEqual res4
+	}
+
+	"Bag normalisation" should "be correct" in {
+		val res1 = Bag("b" -> 1.0)
+		val res2 = Bag("X" -> 0.5, "Y" -> 1.0)
+		val res3 = Bag("X" -> 0.5, "Y" -> 1.0)
+		val res4 = Bag("X" -> 1.0, "Y" -> 0.5555555555555556, "Z" -> 0.3333333333333333)
+		Bag("b" -> 5).normalise shouldEqual res1
+		Bag("X" -> 1, "Y" -> 2).normalise shouldEqual res2
+		Bag("X" -> 0.6, "Y" -> 1.2).normalise shouldEqual res3
+		Bag("X" -> 9.0, "Y" -> 5.0, "Z" -> 3.0).normalise shouldEqual res4
 	}
 }
