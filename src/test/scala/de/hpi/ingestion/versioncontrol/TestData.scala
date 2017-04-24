579c3b2e165e6ca537776d5f51078debaa53c93c
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-04-24 13:43:15 +0200

    Closes #115: Create missing documentation for VersionDiff (#250)
    * Refs #115: refactors code and adds scaladoc
    
    * Refs #115: adds tests
    
    * Refs #115: adds missing tests
    
    * Refs #115: uses system.exit instead of return
    
    * Refs #115: makes recommended changes

---
+++ src/test/scala/de/hpi/ingestion/versioncontrol/TestData.scala
@@ -0,0 +1,151 @@
+package de.hpi.ingestion.versioncontrol
+
+import java.text.SimpleDateFormat
+import java.util.{Date, UUID}
+
+import de.hpi.ingestion.datalake.models.{Subject, Version}
+import de.hpi.ingestion.versioncontrol.models.HistoryEntry
+import play.api.libs.json.{JsValue, Json}
+
+import scala.io.Source
+
+object TestData {
+
+	val timeFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSSZZZZZ")
+
+	def dateOf(date: String): Date = timeFormatter.parse(date)
+
+	def timeUUIDs(): List[UUID] = {
+		List(
+			UUID.fromString("224e1a50-13e2-11e7-9a30-7384674b582f"),
+			UUID.fromString("2195bc70-f6ba-11e6-aa16-63ef39f49c5d"),
+			UUID.fromString("7b410340-243e-11e7-937a-ad9adce5e136"),
+			UUID.fromString("f44df8b0-2425-11e7-aec2-2d07f82c7921"))
+	}
+
+	def timeOfTimeUUIDs(): List[Long] = {
+		List(
+			1490724761717L,
+			1487518996919L,
+			1492523643252L,
+			1492513108923L)
+	}
+
+	def testVersion(version: String, value: List[String], timestamp: String): Version = {
+		val templateVersion = Version(null, "test program", Nil, Map[String, String](), Nil, null)
+		templateVersion.copy(version = UUID.fromString(version), value = value, timestamp = dateOf(timestamp))
+	}
+
+	def versionsToCompare(): (UUID, UUID) = {
+		(timeUUIDs()(1), timeUUIDs()(3))
+	}
+
+	def versions(): List[List[Version]] = {
+		List(
+			List(
+				testVersion("2195bc70-f6ba-11e6-aa16-63ef39f49c5d", List("a", "b"), "2017-02-19 15:43:16.912+0000"),
+				testVersion("224e1a50-13e2-11e7-9a30-7384674b582f", List("b", "c"), "2017-03-28 18:12:41.710+0000")),
+			List(testVersion("2195bc70-f6ba-11e6-aa16-63ef39f49c5d", List("a"), "2017-02-19 15:43:16.912+0000"),
+				testVersion("f44df8b0-2425-11e7-aec2-2d07f82c7921", Nil, "2017-04-18 10:58:28.923+0000"),
+				testVersion("7b410340-243e-11e7-937a-ad9adce5e136", List("c"), "2017-04-18 13:54:03.253+0000")),
+			List(testVersion("2195bc70-f6ba-11e6-aa16-63ef39f49c5d", Nil, "2017-02-19 15:43:16.912+0000"),
+				testVersion("224e1a50-13e2-11e7-9a30-7384674b582f", List("a", "b"), "2017-03-28 18:12:41.710+0000"),
+				testVersion("f44df8b0-2425-11e7-aec2-2d07f82c7921", List("b"), "2017-04-18 10:58:28.923+0000")),
+			List(testVersion("2195bc70-f6ba-11e6-aa16-63ef39f49c5d", List("a", "b"), "2017-02-19 15:43:16.912+0000"),
+				testVersion("224e1a50-13e2-11e7-9a30-7384674b582f", Nil, "2017-03-28 18:12:41.710+0000"),
+				testVersion("f44df8b0-2425-11e7-aec2-2d07f82c7921", List("a", "b"), "2017-04-18 10:58:28.923+0000")),
+			List(testVersion("2195bc70-f6ba-11e6-aa16-63ef39f49c5d", Nil, "2017-02-19 15:43:16.912+0000"),
+				testVersion("224e1a50-13e2-11e7-9a30-7384674b582f", List("b"), "2017-03-28 18:12:41.710+0000"),
+				testVersion("f44df8b0-2425-11e7-aec2-2d07f82c7921", Nil, "2017-04-18 10:58:28.923+0000"),
+				testVersion("7b410340-243e-11e7-937a-ad9adce5e136", List("a"), "2017-04-18 13:54:03.253+0000"))
+		)
+	}
+
+	def foundValues(): List[(List[String], List[String])] = {
+		List(
+			(List("a", "b"), List("b", "c")),
+			(List("a"), Nil),
+			(Nil, List("b")),
+			(List("a", "b"), List("a", "b")),
+			(Nil, Nil))
+	}
+
+	def unorderedVersions(): List[(UUID, UUID)] = {
+		List(
+			(timeUUIDs().head, timeUUIDs()(1)),
+			(timeUUIDs()(1), timeUUIDs().head),
+			(timeUUIDs()(2), timeUUIDs()(2)),
+			(timeUUIDs()(3), timeUUIDs()(1)),
+			(timeUUIDs()(2), timeUUIDs()(3)))
+	}
+
+	def orderedVersions(): List[(UUID, UUID)] = {
+		List(
+			(timeUUIDs()(1), timeUUIDs().head),
+			(timeUUIDs()(1), timeUUIDs().head),
+			(timeUUIDs()(2), timeUUIDs()(2)),
+			(timeUUIDs()(1), timeUUIDs()(3)),
+			(timeUUIDs()(3), timeUUIDs()(2)))
+	}
+
+	def dataLists(): List[Option[(List[String], List[String])]] = {
+		List(
+			Option((List("a", "b"), List("b", "c"))),
+			Option((List("a"), Nil)),
+			Option((Nil, List("b"))),
+			Option((List("a", "b"), List("a", "b"))),
+			None)
+	}
+
+	def listDiffs(): List[Option[JsValue]] = {
+		List(
+			Option(Json.parse("""{"+": ["c"], "-": ["a"]}""")),
+			Option(Json.parse("""{"-": ["a"]}""")),
+			Option(Json.parse("""{"+": ["b"]}""")),
+			None,
+			None)
+	}
+
+	def diffSubjects(): List[Subject] = {
+		List(
+			Subject(
+				id = UUID.fromString("465b3a7a-c621-42ad-a4f2-34e229602989"),
+				name_history = versions().head,
+				aliases_history = versions()(1),
+				category_history = versions()(2),
+				properties_history = Map("test property" -> versions()(3)),
+				relations_history = Map(
+					UUID.fromString("0bb8d6e1-998d-4a8b-8516-c68a4cb4c252") -> Map("test relation" -> versions()(4)))),
+			Subject(
+				id = UUID.fromString("0bb8d6e1-998d-4a8b-8516-c68a4cb4c252"),
+				name_history = versions()(4),
+				aliases_history = versions()(4),
+				category_history = versions()(4),
+				properties_history = Map("test property" -> versions().head),
+				relations_history = Map(
+					UUID.fromString("465b3a7a-c621-42ad-a4f2-34e229602989") -> Map("test relation" -> versions()(1)))))
+	}
+
+	def jsonDiff(): List[JsValue] = {
+		val rawJson = Source.fromURL(getClass.getResource("/versioncontrol/diff.json")).getLines().mkString("\n")
+		Json.parse(rawJson).as[List[JsValue]]
+	}
+
+	def historyEntries(): List[HistoryEntry] = {
+		List(
+			HistoryEntry(
+				UUID.fromString("465b3a7a-c621-42ad-a4f2-34e229602989"),
+				dataLists().head,
+				dataLists()(1),
+				dataLists()(2),
+				Map("test property" -> dataLists()(3)),
+				Map(UUID.fromString("0bb8d6e1-998d-4a8b-8516-c68a4cb4c252") -> Map("test relation" -> dataLists()(4)))),
+			HistoryEntry(
+				UUID.fromString("0bb8d6e1-998d-4a8b-8516-c68a4cb4c252"),
+				None,
+				None,
+				None,
+				Map("test property" -> dataLists().head),
+				Map(UUID.fromString("465b3a7a-c621-42ad-a4f2-34e229602989") -> Map("test relation" -> dataLists()(1)))))
+	}
+}
--- src/test/scala/de/hpi/ingestion/versioncontrol/VersionDiffTest.scala
+++ src/test/scala/de/hpi/ingestion/versioncontrol/VersionDiffTest.scala
@@ -1,70 +1,72 @@
 package de.hpi.ingestion.versioncontrol
 
-import org.scalatest.FlatSpec
-import com.holdenkarau.spark.testing.SharedSparkContext
-import org.apache.spark.rdd.RDD
+import org.scalatest.{FlatSpec, Matchers}
+import com.holdenkarau.spark.testing.{RDDComparisons, SharedSparkContext}
 import java.util.UUID
 
-class VersionDiffTest extends FlatSpec with SharedSparkContext {
+class VersionDiffTest extends FlatSpec with SharedSparkContext with Matchers with RDDComparisons {
 
     val oldVersion = UUID.fromString("fc2c5a40-c566-11e6-aee2-5f2c06e3b302")
     val newVersion = UUID.fromString("7ce032b0-c567-11e6-8252-5f2c06e3b302")
-/*
-    "retrieved versions" should "contain exactly two elements" in {
-        val versionRDD = subjectVersionRDD()
-            .map(VersionDiff.retrieveVersions(_, oldVersion, newVersion))
 
-        val invalidVersions = versionRDD
-            .filter { entry =>
-                var invalidity = false
-                for(element <- entry) {
-                    element match {
-                        case el : List[Version] =>
-                            if(element != null) {
-                                if(element.size != 2)
-                                    invalidity = true
-                            }
-                        case el : Map[String, List[Version]] =>
-                            for((key, value) <- el) {
-                                if(value != null) {
-                                    if(value.size != 2)
-                                        invalidity = true
-                                }
-                            }
-                        case el : Map[UUID, Map[String, List[Version]]] =>
-                            for((key, value) <- el) {
-                                for((key2, value2) <- value) {
-                                    if(value2 != null) {
-                                        if(value2.size != 2)
-                                            invalidity = true
-                                    }
-                                }
-                            }
-                        case _ => {}
-                    }
-                }
-                invalidity
-            }
-        assert(invalidVersions.count == 0 && versionRDD.count > 0)
-    }
+	"Version diff" should "be created" in {
+		val subjects = sc.parallelize(TestData.diffSubjects())
+		val (oldV, newV) = TestData.versionsToCompare()
+		val versions = Array(oldV.toString, newV.toString)
+		val versionDiff = VersionDiff.run(subjects, versions)
+		val expectedDiff = sc.parallelize(TestData.jsonDiff())
+		assertRDDEquals(versionDiff, expectedDiff)
+	}
 
-    "retrieved versions" should "contain exactly two elements" in {
-        val jsonRDD = subjectVersionRDD()
-            .map(VersionDiff.retrieveVersions(_, oldVersion, newVersion))
-            .map(VersionDiff.diffToJson)
+	"Versions" should "be ordered correctly" in {
+		val orderedVersions = TestData.unorderedVersions()
+			.map((VersionDiff.versionOrder _).tupled)
+		val expectedVersions = TestData.orderedVersions()
+		orderedVersions shouldEqual expectedVersions
+	}
 
-        val invalidVersions = jsonRDD
-            .map(Json.parse(_))
+	"Value lists of a Subject" should "be retrieved from the version lists" in {
+		val (oldV, newV) = TestData.versionsToCompare()
+		val retrievedData = TestData.diffSubjects()
+			.map(VersionDiff.retrieveVersions(_, oldV, newV))
+		val expectedData = TestData.historyEntries()
+		retrievedData shouldEqual expectedData
+	}
 
-        assert(invalidVersions.count == 0 && versionRDD.count > 0)
-    }
-*/
+	"Correct values" should "be retrieved from version list" in {
+		val (oldV, newV) = TestData.versionsToCompare()
+		val fieldValues = TestData.versions()
+			.map(versionList => (VersionDiff.findValues(oldV, versionList), VersionDiff.findValues(newV, versionList)))
+		val expectedValues = TestData.foundValues()
+		fieldValues shouldEqual expectedValues
+	}
 
+	"History entry" should "be diffed and serialized as JSON" in {
+		val diff = TestData.historyEntries()
+			.map(VersionDiff.diffToJson)
+		val expectedDiff = TestData.jsonDiff()
+		diff shouldEqual expectedDiff
+	}
 
-    // extracted from WikiData dump
-    def subjectVersionRDD(): RDD[String] = {
-        sc.parallelize(List(
+	"Value lists" should "be created from two versions" in {
+		val (oldV, newV) = TestData.versionsToCompare()
+		val valueLists = TestData.versions()
+			.map(VersionDiff.createValueList(oldV, newV, _))
+		val expectedLists = TestData.dataLists()
+		valueLists shouldEqual expectedLists
+	}
 
-        ))
-    }
+	they should "be diffed and serialized as JSON" in {
+		val valueDiffs = TestData.dataLists()
+			.map(VersionDiff.diffLists)
+		val expectedDiffs = TestData.listDiffs()
+		valueDiffs shouldEqual expectedDiffs
+	}
+
+	"Time" should "be extracted from UUID" in {
+		val uuidTimes = TestData.timeUUIDs()
+			.map(VersionDiff.timeFromUUID)
+		val expectedTimes = TestData.timeOfTimeUUIDs()
+		uuidTimes shouldEqual expectedTimes
+	}
 }
